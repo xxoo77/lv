e9c508fafb4ac04c5b7b39dff415dc4a61efe9d9
xxoo77 <77449410+xxoo77@users.noreply.github.com>
2021-01-18 23:12:08 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -32,6 +32,11 @@ $(function() {
 			'url':'http://www.ccav18.net/go/66',
 		},
 		{
+			'icon':'https://pbs.twimg.com/profile_images/1217861599395905538/9Mlmh5_p_400x400.jpg',
+			'name':'香蕉',
+			'url':'https://www.anxnr.com/downloadEx/index.html?invite_code=S0HBMB&_t=kk2pbz12&',
+		},
+		{
 			'icon':'https://www.yy2.lv/uploads/20200912/d59cb0a6a54172068ce4ea1586363963.jpg',
 			'name':'泡芙',
 			'url':'https://ppffz.me?dc=MH',
