6bf049d13bda42e00c102078e55e2ba9bcf4dcc7
xxoo77 <77449410+xxoo77@users.noreply.github.com>
2021-01-31 13:08:14 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -88,8 +88,8 @@ $(function() {
 		},
 		{
 			'icon':'https://www.landenet.net/uploadfile/2016/0528/20160528102637548.png',
-			'name':'密码cad3',
-			'url':'https://yunpan.360.cn/surl_ySDSspbJVQ4',
+			'name':'密码5e9b',
+			'url':'https://yunpan.360.cn/surl_ySzcFvcDDkX',
 		},
 	];
 	
