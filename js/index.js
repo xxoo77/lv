34b7f9d88f62c23efdd39aedda24f2c6c4b94fa2
xxoo77 <77449410+xxoo77@users.noreply.github.com>
2021-02-04 00:37:37 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,113 +1,159 @@
 $(function() {
+	var classify = ['福利','直播','攻略'];
 	var navlist = [
 		
 		{
-			'icon':'https://www.yy2.lv/uploads/20201219/d984667dbd0561e5b9957a2de5f8e634.jpg',
+			'icon':'images/1.jpg',
 			'name':'爸爸点我',
 			'url':'https://mtvdtd.xyz?ch=mu',
+			'size': 0
 		},
 		{
-			'icon':'https://www.18se.biz/upimg/applogos/20200905/20200905214109755.gif',
+			'icon':'images/2.gif',
 			'name':'麻豆推荐',
 			'url':'https://mdapp07.com/?_c=sqs00058',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200724/e41838ef67b29404b5681debe6cf291a.jpg',
+			'icon':'images/3.jpg',
 			'name':'草榴',
 			'url':'https://spavrx.xyz?ch=mu',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200908/e6162d1d515d153d3b6162cddedcbc47.jpg',
+			'icon':'images/4.jpg',
 			'name':'抖阴',
 			'url':'https://20dy.me?channel=dy20cps',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200825/a3e129f529a4a5df2a29fe627d355b90.jpg',
+			'icon':'images/5.jpg',
 			'name':'抖阴国际',
 			'url':'https://dou1.me?channel=dy204zc',
+			'size': 0
 		},
 		{
-			'icon':'https://www.18se.biz/upimg/applogos/20200928/20200928150856424.gif',
+			'icon':'images/6.gif',
 			'name':'楼凤',
 			'url':'https://site.tea123.me/?code=eTdu&c=333',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200928/b4f8c647078178590affcadf6815d8be.png',
+			'icon':'images/7.png',
 			'name':'杏吧',
 			'url':'http://www.ccav18.net/go/66',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200826/72bafc1de0447b3252029ff856d49542.jpg',
+			'icon':'images/8.jpg',
 			'name':'香蕉',
 			'url':'https://www.anxnr.com/downloadEx/index.html?invite_code=50HBM2&_t=kk3ivq0e&',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200912/d59cb0a6a54172068ce4ea1586363963.jpg',
+			'icon':'images/9.jpg',
 			'name':'泡芙',
 			'url':'https://ppffz.me?dc=MH',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200724/401535e9c034d8780908fff837ecf1f2.png',
+			'icon':'images/10.png',
 			'name':'约炮神器',
 			'url':'https://yhacc.live?dc=MH',
+			'size': 0
 		},
 		{
-			'icon':'https://www.18se.biz/upimg/applogos/20201201/20201201210841258.png',
+			'icon':'images/11.png',
 			'name':'色中色',
 			'url':'https://51sis.vip?dc=MH',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20201126/1a711951de5c986df73332fa49230785.jpg',
+			'icon':'images/12.jpg',
 			'name':'精东',
 			'url':'http://jdhot1.me?_c=jd2108s',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200817/842846dfbd4ef878c8d98dd0b822da84.png',
+			'icon':'images/13.png',
 			'name':'快手成人',
 			'url':'https://zb.91tvapi.me/chan/k10165/a5GN8',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200817/47275852694037ef8905494f7a387a25.png',
+			'icon':'images/14.png',
 			'name':'91AV',
 			'url':'https://app.aff91.com/chan-1108/aff-axa2B',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200817/df5444436599f60bd8b1eea305a8ee4a.png',
+			'icon':'images/15.png',
 			'name':'91滴滴',
 			'url':'https://download.91didi.xyz/chan-1025/aff-jbyC',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200817/73daeab17d4c70e0097874298ed3ee3f.png',
+			'icon':'images/16.png',
 			'name':'汤头条',
 			'url':'https://ttt.tangbr.net/chan/a10141/xme3',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200817/e9d6dde82d77cc8368a96917d68c5a3f.png',
+			'icon':'images/17.png',
 			'name':'蚂蚁VPN',
 			'url':'https://web.lanshuapi.com/c-333/a-a2Xkf',
+			'size': 0
 		},
 		{
-			'icon':'https://www.yy2.lv/uploads/20200817/d298eca0ac93acc0bae730978ac8fc69.png',
+			'icon':'images/18.png',
 			'name':'91短视频',
 			'url':'https://download.91porn005.me/chan-1130/aff-dR2T7',
+			'size': 0
 		},
 		{
-			'icon':'https://www.landenet.net/uploadfile/2016/0528/20160528102637548.png',
+			'icon':'images/19.png',
 			'name':'密码2028',
 			'url':'https://yunpan.360.cn/surl_ySza7AcXmcU',
+			'size': 0
+		},
+		{
+			'icon':'images/2-1.png',
+			'name':'点爱攻略',
+			'url':'strategy.html',
+			'size': 2
 		},
 	];
-	
-	$.each(navlist,function(i,e){
-		var html = '<li class="nav-item">';
-			html += '<img src="'+e.icon+'" alt="" class="icon">';
-			html += '<span class="name">'+e.name+'</span>';
+	var urllist = [];
+	addApp(0);
+	$.each(classify,function(i,e){
+		var html = '<li class="class-item">';
+			html += e;
 			html += '</li>';
 		var content = $(html);
-		$('.navlist').append(content);
+		$('.classify').append(content);
 	});
 	$('.navlist').on('click','.nav-item',function(){
-		console.log(navlist[$(this).index()].url);
-		window.location.href = navlist[$(this).index()].url;
+		window.location.href = urllist[$(this).index()];
 	});
+	$('.classify').on('click','.class-item',function(){
+		var clickthis = $(this);
+		clickthis.addClass('active').siblings().removeClass('active');
+		var index = clickthis.index();
+		$('.navlist').empty();
+		addApp(index);
+	});
+	function addApp(size){
+		urllist = [];
+		$.each(navlist,function(i,e){
+			if(e.size == size){
+				urllist.push(navlist[i].url);
+				var html = '<li class="nav-item">';
+					html += '<img src="'+e.icon+'" alt="" class="icon">';
+					html += '<span class="name">'+e.name+'</span>';
+					html += '</li>';
+				var content = $(html);
+				$('.navlist').append(content);
+			}
+		});
+	}
 });
